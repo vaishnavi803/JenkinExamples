3b27a5c44d341cacb1c9d1205f1767e2157bcceb
vaishnavi803 <ashdevatha7@gmail.com>
2025-03-28 13:24:54 +0530

    Update Demo1.java

--- Demo1.java
+++ Demo1.java
@@ -3,5 +3,7 @@ class Demo1
 public static void  main(String args[])
 {
 System.out.println("Hello World");
+  System.out.println("Hello Vaish :))");
+  System.out.println("Hello 245122737058");
 }
 }
