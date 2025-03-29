ec21d2b0f4ebb90a68d1f901e84d6c40bf7ec2ba
vaishnavi803 <ashdevatha7@gmail.com>
2025-03-29 12:28:28 +0530

    Add files via upload

---
+++ CommandLineDemo2.java
@@ -0,0 +1,13 @@
+class CommandLineDemo2
+{
+public static void main(String args[])
+{
+ String name=args[0];
+ int rollno=Integer.parseInt(args[1]);
+ 
+ System.out.println("Name: "+name);
+ System.out.println("Rollno: "+rollno);
+ 
+ }
+}
+
